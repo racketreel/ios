c37bf1001788aa7f2487bf19a39f00e139ef9a6e
tom-elvidge <tomelvidge1999@gmail.com>
2021-08-04 22:00:19 +0100

    Added rough video picker

--- Game Set Match/View/MatchView.swift
+++ Game Set Match/View/MatchView.swift
@@ -11,6 +11,9 @@ struct MatchView: View {
     
     let match: Match
     
+    @State private var showImagePicker = false
+    @State private var inputImage: UIImage?
+    
     var body: some View {
         List {
             ForEach ((match.history!.array as! [MatchState]), id: \.self) { matchState in
@@ -30,18 +33,69 @@ struct MatchView: View {
             }
         }
         .toolbar {
-            Button("Cut Video", action: {
-                // Todo
-                print("Cutting video...")
+            Button("Process Video", action: {
+                self.showImagePicker = true
             })
         }
         .navigationTitle(String((match.history!.array[0] as! MatchState).generationEventTimestamp))
         .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $showImagePicker, onDismiss: cut) {
+            ImagePicker(image: self.$inputImage)
+                .ignoresSafeArea()
+        }
     }
+    
+    // Temp - move to new ViewModel
+    func cut() {
+        print("Cutting")
+    }
+    
 }
 
 struct MatchView_Previews: PreviewProvider {
     static var previews: some View {
         MatchView(match: Match())
     }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+    }
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.mediaTypes = ["public.movie"]
+        picker.allowsEditing = false
+        // Longest ever tennis match 11 hours and 5 minutes (39900 seconds)
+        picker.videoMaximumDuration = 39900
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
 }
